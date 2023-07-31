9c1b50b2bb1c54ffc23d19973a259b77f7ced7b9
timbednar <tim.bednar@gmail.com>
2023-07-31 15:11:37 -0500

    sheet-17 fix youtube videos

--- web/themes/custom/ellison/components/01-atoms/videos/video.js
+++ web/themes/custom/ellison/components/01-atoms/videos/video.js
@@ -5,23 +5,28 @@ Drupal.behaviors.responsiveEmbeddedVideos = {
     // any given element one time at most, regardless of how many times
     // the behaviour itself is called (it is not sufficient in general
     // to assume an element will only ever appear in a single context).
-    once('enableResponsiveEmbeddedVideo', 'iframe[src*="vimeo.com"]', context).forEach(
-      
+
+    function makeResponsive(element) {
+      const width = element.getAttribute('width');
+      const height = element.getAttribute('height');
+      const aspectRatio = `${width} / ${height}`;
+      const parentDiv = element.parentNode;
+      parentDiv.style.aspectRatio = aspectRatio;
+      // Clear static height/width attributes for responsive styles
+      element.setAttribute('height','100%');
+      element.setAttribute('width','100%');
+    }
+  
+    once('enableResponsiveEmbeddedVimeo', 'iframe[src*="vimeo.com"]', context).forEach(
       function (element) {
-
-        const width = element.getAttribute('width');
-        const height = element.getAttribute('height');
-        const aspectRatio = `${width} / ${height}`;
-        const parentDiv = element.parentNode;
-
-        parentDiv.style.aspectRatio = aspectRatio;
-
-        // Clear static height/width attributes for responsive styles
-        element.setAttribute('height','100%');
-        element.setAttribute('width','100%');
-        
+        makeResponsive(element);
       }
-      
+    );
+
+    once('enableResponsiveEmbeddedYouTube', 'iframe[src*="youtu.be"]', context).forEach(
+      function (element) {
+        makeResponsive(element);
+      }
     );
 
   }
