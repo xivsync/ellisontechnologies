9155a45555ef00294c805d4e633e6d89f934102c
timbednar <tim.bednar@gmail.com>
2023-08-07 20:04:01 -0500

    sheet-201 newsletter

--- web/sites/default/settings.ddev.php
+++ web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'ezLrguMDdWddQEFVbudZohBLRScCCDQqHHbJHndoBpRaoGCVickmpmMVdLnozKif';
+$settings['hash_salt'] = 'XCzVdiuFDIrXEpvMscGMQnFMAZtYQtMXycnCkYAmHXcQZwJZHpScVOcPpTsYUZcP';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
