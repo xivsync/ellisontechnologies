3a488136bc1ac5c15755301286199d2bbe1b22fb
timbednar <tim.bednar@onespan.com>
2023-10-28 18:30:41 -0500

    Initial main navigation

---
+++ web/themes/custom/ellison/components/02-molecules/menus/main-navigation/main-navigation.js
@@ -0,0 +1,73 @@
+Drupal.behaviors.mainMenu = {
+  attach: function (context, settings) {
+    // Use context to filter the DOM to only the elements of interest,
+    // and use once() to guarantee that our callback function processes
+    // any given element one time at most, regardless of how many times
+    // the behaviour itself is called (it is not sufficient in general
+    // to assume an element will only ever appear in a single context).
+    once('handleMainMenu', 'div#main-menu', context).forEach(
+
+      function (element) {
+
+        const toggleExpand = context.getElementById('toggle-expand');
+        const menu = context.getElementById('main-nav');
+        if (menu) {
+          const expandMenu = menu.getElementsByClassName('expand-sub');
+    
+          // Mobile Menu Show/Hide.
+          toggleExpand.addEventListener('click', (e) => {
+            toggleExpand.classList.toggle('toggle-expand--open');
+            menu.classList.toggle('main-nav--open');
+            e.preventDefault();
+          });
+    
+          // Expose mobile sub menu on click.
+          for (let i = 0; i < expandMenu.length; i += 1) {
+            expandMenu[i].addEventListener('click', (e) => {
+              const menuItem = e.currentTarget;
+              const subMenu = menuItem.nextElementSibling;
+    
+              menuItem.classList.toggle('expand-sub--open');
+              subMenu.classList.toggle('main-menu--sub-open');
+            });
+          }
+        }
+        
+      }
+      
+    );
+
+    // Enable mobile
+    once('handleMobi', '.header__mobi-foo', context).forEach(
+      
+      
+      function (element) {
+
+        function toggleMobi(e){
+
+          const buttonEl = e.target;
+          const toggleTarget = e.target.getAttribute('data-toggle-target');
+          const toggleTargetEl = document.getElementsByClassName(toggleTarget)[0];
+          const isExpanded = buttonEl.getAttribute('aria-expanded');
+    
+          if (isExpanded === 'true') {
+            toggleTargetEl.classList.remove('expanded');
+            buttonEl.setAttribute('aria-expanded', false);
+          } else {
+            toggleTargetEl.classList.add('expanded');
+            buttonEl.setAttribute('aria-expanded', true);
+          }
+    
+          e.preventDefault();
+        }
+
+        const buttonEls = element.getElementsByTagName('button');
+        for (buttonEl of buttonEls) {
+          buttonEl.addEventListener("click", toggleMobi);
+        }
+
+      }
+    );
+
+  }
+};
