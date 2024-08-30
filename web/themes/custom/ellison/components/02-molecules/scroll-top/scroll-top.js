6c35855ed7a4c7ff6cf8f2171650ae7c52801850
Tim Bednar <tim.bednar@trinetsolutions.com>
2024-08-30 15:23:16 -0500

    Update video view and remove captcha from newsletter form

--- web/themes/custom/ellison/components/02-molecules/scroll-top/scroll-top.js
+++ web/themes/custom/ellison/components/02-molecules/scroll-top/scroll-top.js
@@ -1,33 +1,26 @@
 (function ($, Drupal) {
-Drupal.behaviors.scrollTop = {
-    attach: function(context, settings) {
-        once('scrollTop', 'body', context).forEach(function(element) {
-            console.log('loaded');
-            $(window).on('load',function() {
-                // if("#topScroll") {
-                //     $("#topScroll").remove();
-                //     console.log('Removed');
-                // }
-                $('body').append('<div id="topScroll"><button class="button" type="submit"><img src="/themes/custom/ellison/images/icons/gototop.png"></div>');
-                // if('#topScroll') {
-                //     console.log("Button Added");
-                // }
-                $('.button').click(function() {
-                    $("html, body").animate({scrollTop: 0},1000);
-                    console.log("Button Pressed");
-                });
-                $('#topScroll').hide();
-            })
-            // Conditions for hiding and showing button
-            $(window).scroll(function() {
-            if($(window).scrollTop() < 200){
-                $('#topScroll').hide(600);
-                // console.log("Button Hidden");
+    Drupal.behaviors.scrollTop = {
+      attach: function (context, settings) {
+        once('scrollTop', 'body', context).forEach(function (element) {
+  
+          $(window).on('load', function () {
+            $('body').append('<div id="topScroll"><button class="button" type="submit"><img src="/themes/custom/ellison/images/icons/gototop.png"></div>');
+            $('#topScroll').click(function () {
+              $("html, body").animate({ scrollTop: 0 }, 1000);
+            });
+            $('#topScroll').hide();
+          });
+  
+          // Conditions for hiding and showing button
+          $(window).scroll(function () {
+            if ($(window).scrollTop() < 200) {
+              $('#topScroll').hide(600);
+            } else if ($(window).scrollTop() > 200 && $('#topScroll')[0]) {
+              $('#topScroll').show(600);
             }
-            else if($(window).scrollTop()>200 && $('#topScroll')[0]) {
-                $('#topScroll').show(600);
-                // console.log("Button Shown")
-        }});
-        })
-    }
-}; }) (jQuery, Drupal);
+          });
+
+        });
+      }
+    };
+  })(jQuery, Drupal);
