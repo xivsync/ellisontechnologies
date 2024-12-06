adfe135a96afa133a7d5d326977fbf5813950cd8
Tim Bednar <tim.bednar@trinetsolutions.com>
2024-12-06 15:08:07 -0600

    Sorting model names

--- web/themes/custom/ellison/components/02-molecules/dropdown/dropdown.js
+++ web/themes/custom/ellison/components/02-molecules/dropdown/dropdown.js
@@ -34,5 +34,46 @@ Drupal.behaviors.dropdown = {
       
     );
 
+    once('sortDropdown', '.series-list .dropdown__menu--list', context).forEach(
+      
+      function (element) {
+
+        console.log(element);
+        var list, i, switching, b, shouldSwitch;
+        list = element;
+
+        switching = true;
+        /* Make a loop that will continue until
+        no switching has been done: */
+        while (switching) {
+          // start by saying: no switching is done:
+          switching = false;
+          b = list.getElementsByTagName("LI");
+          // Loop through all list-items:
+          for (i = 0; i < (b.length - 1); i++) {
+            // start by saying there should be no switching:
+            shouldSwitch = false;
+            /* check if the next item should
+            switch place with the current item: */
+            if (b[i].innerText.toLowerCase() > b[i + 1].innerText.toLowerCase()) {
+              /* if next item is alphabetically
+              lower than current item, mark as a switch
+              and break the loop: */
+              shouldSwitch = true;
+              break;
+            }
+          }
+          if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark the switch as done: */
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+          }
+        }
+        
+      }
+      
+    );
+
   }
 };
